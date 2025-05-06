32709cd0f5dbf54c560af56781f78fd36bbe0499
Abhijeet Mavi <abhijeetmavi999@gmail.com>
2025-05-06 16:38:28 +0530

    added datepicker and table

---
+++ src/test/java/tables/DynamicTableWithPagination.java
@@ -0,0 +1,17 @@
+package tables;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class DynamicTableWithPagination {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("https://demo.opencart.com/admin/index.php");
+        driver.manage().window().maximize();
+
+    }
+}
